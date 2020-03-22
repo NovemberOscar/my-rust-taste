baf19629fdd9c401e82a1636496e59ad10b151df
Seonghyeon Kim <kim@seonghyeon.dev>
2020-03-22 17:12:33 +0900

    Use stderr to show error

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing args: {}", err);
+        eprintln!("Problem parsing args: {}", err);
         process::exit(1);
     });
 
@@ -17,7 +17,7 @@ fn main() {
     println!("In file {}", config.filename);
 
     if let Err(e) = run(config) {
-        println!("Something went wrong!:\n{}", e);
+        eprintln!("Something went wrong!:\n{}", e);
 
         process::exit(1);
     };
