b1a62572255c1e28e55d5e864010883ae32e4a25
Seonghyeon Kim <kim@seonghyeon.dev>
2020-03-22 15:02:28 +0900

    Separate logics to lib.rs

---
+++ chapter12/minigrep/src/main.rs
@@ -0,0 +1,24 @@
+extern crate minigrep;
+
+use std::env;
+use std::process;
+
+use minigrep::{run, Config};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing args: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Something went wrong!:\n{}", e);
+
+        process::exit(1);
+    };
+}
