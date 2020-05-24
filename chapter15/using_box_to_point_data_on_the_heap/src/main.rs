7abb0275432ef76bec676f61c70c771b606acb58
Seonghyeon Kim <self@seonghyeon.dev>
2020-05-24 21:03:09 +0900

    Using Box<T> to make ConsList representable

--- chapter15/using_box_to_point_data_on_the_heap/src/main.rs
+++ chapter15/using_box_to_point_data_on_the_heap/src/main.rs
@@ -1,10 +1,10 @@
 use crate::ConsList::{Cons, Nil};
 
 enum ConsList {
-    Cons(i32, ConsList),
+    Cons(i32, Box<ConsList>),
     Nil,
 }
 
 fn main() {
-    let cons_ls = Cons(1, Cons(2, Cons(3, Nil)));
+    let cons_ls = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
