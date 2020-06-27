97b9ca1578ccd524ffafa2cf107da4704fb5ce9f
Seonghyeon Kim <self@seonghyeon.dev>
2020-06-27 23:03:33 +0900

    Validating the Request and Selectively Responding

--- chapter20/singlethread_ws/src/main.rs
+++ chapter20/singlethread_ws/src/main.rs
@@ -16,18 +16,35 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    let mut file = File::open(
-        "/Users/k.seonghyeon/Documents/GitHub/my-rust-taste/chapter20/singlethread_ws/src/hello.html",
-    )
-    .unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
+    let http_get = b"GET / HTTP/1.1\r\n";
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\n{}",
-        contents
-    );
+    if buffer.starts_with(http_get) {
+        let mut file = File::open(
+            "/Users/k.seonghyeon/Documents/GitHub/my-rust-taste/chapter20/singlethread_ws/src/hello.html",
+        ).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\n{}",
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let mut file = File::open(
+            "/Users/k.seonghyeon/Documents/GitHub/my-rust-taste/chapter20/singlethread_ws/src/404.html",
+        ).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+
+        let response = format!(
+            "HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html; charset=UTF-8\r\n\n{}",
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
