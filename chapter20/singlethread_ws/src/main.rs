2060ef7429164b65568d290217067703afefb969
Seonghyeon Kim <self@seonghyeon.dev>
2020-06-27 22:22:34 +0900

    Returning Real HTML

--- chapter20/singlethread_ws/src/main.rs
+++ chapter20/singlethread_ws/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
@@ -13,10 +14,19 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=UTF-8\r\n\nHello World!";
+    let mut file = File::open(
+        "/Users/k.seonghyeon/Documents/GitHub/my-rust-taste/chapter20/singlethread_ws/src/hello.html",
+    )
+    .unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\n{}",
+        contents
+    );
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
