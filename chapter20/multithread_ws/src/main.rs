26208a7d91e49d11893a539a7b6858f4c0a66b00
Seonghyeon Kim <self@seonghyeon.dev>
2020-06-28 13:30:18 +0900

    Simulating a Slow Request in the Current Server Implementation

---
+++ chapter20/multithread_ws/src/main.rs
@@ -0,0 +1,45 @@
+use std::fs::File;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let index_get = b"GET / HTTP/1.1\r\n";
+    let sleep_get = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(index_get) {
+        ("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\n",
+         "/Users/k.seonghyeon/Documents/GitHub/my-rust-taste/chapter20/singlethread_ws/src/hello.html")
+    } else if buffer.starts_with(sleep_get) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\n",
+         "/Users/k.seonghyeon/Documents/GitHub/my-rust-taste/chapter20/singlethread_ws/src/hello.html")
+    }
+    else {
+        ("HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html; charset=UTF-8\r\n\n",
+         "/Users/k.seonghyeon/Documents/GitHub/my-rust-taste/chapter20/singlethread_ws/src/404.html")
+    };
+
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
